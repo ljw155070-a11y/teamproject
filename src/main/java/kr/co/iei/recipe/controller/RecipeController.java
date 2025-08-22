592c704084d63ed624dfa40f68dc8f548d5b71ca
ljw155070-a11y <ljw155070@gmail.com>
2025-08-22 23:20:38 +0900

    index.html 경로 수정 및 레시피 html 작업용 이동 경로 생성

--- src/main/java/kr/co/iei/recipe/controller/RecipeController.java
+++ src/main/java/kr/co/iei/recipe/controller/RecipeController.java
@@ -1,7 +1,8 @@
 package kr.co.iei.recipe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.recipe.model.service.RecipeService;
@@ -11,4 +12,9 @@ import kr.co.iei.recipe.model.service.RecipeService;
 public class RecipeController {
 	@Autowired
 	private RecipeService recipeService;
+	
+	@GetMapping(value="/list")
+	public String recipeList(Model model) {
+		return "recipe/list";
+	}
 }
