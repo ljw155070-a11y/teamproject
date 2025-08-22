9abd0ed60e7c4187e7fbfda4c051e947917c785d
ljw155070-a11y <ljw155070@gmail.com>
2025-08-22 22:58:45 +0900

    레시피 컨트롤러 서비스 오토와이어드 추가

--- src/main/java/kr/co/iei/recipe/controller/RecipeController.java
+++ src/main/java/kr/co/iei/recipe/controller/RecipeController.java
@@ -1,10 +1,14 @@
 package kr.co.iei.recipe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.iei.recipe.model.service.RecipeService;
 
 @Controller
 @RequestMapping(value="/recipe")
 public class RecipeController {
-
+	@Autowired
+	private RecipeService recipeService;
 }
--- src/main/java/kr/co/iei/recipe/model/service/RecipeService.java
+++ src/main/java/kr/co/iei/recipe/model/service/RecipeService.java
@@ -1,8 +1,12 @@
 package kr.co.iei.recipe.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.co.iei.recipe.model.dao.RecipeDao;
 
 @Service
 public class RecipeService {
-
+	@Autowired
+	private RecipeDao recipeDao;
 }
