d8d694019bb213607bfbcc392138cf2129a8f0fd
chans0928-alt <chans0928@gmail.com>
2025-08-23 16:36:15 +0900

    allMember list작업

--- src/main/java/kr/co/iei/admin/AdminController.java
+++ src/main/java/kr/co/iei/admin/AdminController.java
@@ -1,8 +1,24 @@
 package kr.co.iei.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.iei.member.model.service.MemberService;
 
 @Controller
+@RequestMapping(value="/admin")
 public class AdminController {
-
+	@Autowired
+	private MemberService memberService;
+	@GetMapping(value="/allMember")
+	public String allMember(Model model) {
+		List list = memberService.selectAllMember();
+		model.addAttribute("list", list);
+		return "admin/allMember";
+	}
 }
