cf6502892ec7d37263de3d868b72a47828b30665
tlxmf97-hash <tlxmf97@gmail.com>
2025-08-25 14:11:17 +0900

    커밋

--- src/main/java/kr/co/iei/qna/controller/QnaController.java
+++ src/main/java/kr/co/iei/qna/controller/QnaController.java
@@ -2,13 +2,24 @@ package kr.co.iei.qna.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.qna.model.service.QnaService;
+import kr.co.iei.qna.model.vo.Qna;
 
 @Controller
 @RequestMapping(value="/qna")
 public class QnaController {
 	@Autowired
 	private QnaService qnaService;
+	
+	/*@GetMapping(value="/qnaList") //qna 페이징
+	public String qnaList(int reqPage, Model model) {
+		Qna qna= qnaService.selectQnaList(reqPage);
+		return "qna/list";
+	}
+	*/
+	
 }
