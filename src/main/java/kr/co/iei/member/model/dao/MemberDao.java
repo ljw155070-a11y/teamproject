f9afb121383b8bed7dbfcfae989595360bfd84a8
chans0928-alt <chans0928@gmail.com>
2025-08-26 15:44:56 +0900

    크아악

--- src/main/java/kr/co/iei/member/model/dao/MemberDao.java
+++ src/main/java/kr/co/iei/member/model/dao/MemberDao.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.co.iei.member.model.vo.Member;
+
 @Mapper
 public interface MemberDao {
 
-	List selectAllMember();
 
 	int selectMemberTotalCount();
+
+	List<Member> selectMemberList(int start, int end);
 
 }
--- src/main/java/kr/co/iei/member/model/service/MemberService.java
+++ src/main/java/kr/co/iei/member/model/service/MemberService.java
@@ -2,24 +2,20 @@ package kr.co.iei.member.model.service;
 
 import java.util.HashMap;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.admin.AdminController;
 import kr.co.iei.member.model.dao.MemberDao;
-import kr.co.iei.member.model.vo.MemberListData;
+import kr.co.iei.member.model.vo.Member;
 
 @Service
 public class MemberService {
 	
-	
 	@Autowired
-	private MemberDao memberDao;
-	
-	
+	private MemberDao memberDao;	
 
-	public MemberListData selectMemberList(int reqPage) {
+	public HashMap<String, Object> selectMemberList(int reqPage) {
 		
 		//reqPage = 사용자가 요청한 페이지 번호
 		
@@ -30,24 +26,54 @@ public class MemberService {
 		int end = reqPage * numPerPage;
 		int start = end - numPerPage;
 		
-		HashMap<String, Object> param = new HashMap<String, Object>();
-		param.put("end", end);
-		param.put("start", start);
+		List<Member> list = memberDao.selectMemberList(start, end);
+		
+		HashMap<String, Object> reqSet = new HashMap<>();
+		
 		
 		//전체 게시물 수
 		int totalCount = memberDao.selectMemberTotalCount();
 		
+		HashMap<String, Integer> pageInfo = new HashMap<>();
 		//전체 페이지 수
 		int totalPage = (int)(Math.ceil(totalCount/(double)numPerPage));
 		
 		//페이지네비 길이
 		int pageNaviSize = 5;
 		
-		//페이지네비 시작번호
-		int pageNo = reqPage-2;
+		//양쪽에 올 네비 갯수
+		int bothSidePage = (pageNaviSize-1)/2;
+		
+		int startNo = Math.max(1, bothSidePage);
+		
+		int endNo = Math.min(totalPage, reqPage+bothSidePage);
+		
+		if(totalPage <= pageNaviSize) {
+			startNo = 1;
+			endNo = totalPage;
+		}else {
+			if((reqPage-bothSidePage)<1) {
+				endNo += (1-(reqPage-bothSidePage));
+			}else if((reqPage+bothSidePage)>totalPage) {
+				startNo -= (reqPage+bothSidePage)-totalPage; 
+			}
+		}
+		pageInfo.put("reqPage", reqPage);
+		pageInfo.put("startNo", startNo);
+		pageInfo.put("endNo", endNo);
+		pageInfo.put("totalCount", totalCount);
+		pageInfo.put("totalPage", totalPage);
+		
+		reqSet.put("pageInfo", pageInfo);
+		reqSet.put("list", list);
+		
+		HashMap<String,Integer> test = (HashMap<String,Integer>)reqSet.get("pageInfo");
 		
 		
-		return null;
+		List<Member> test2 = (List<Member>)reqSet.get("list");
+		
+		
+		return reqSet;
 	}
 
 }
