dbc0b743bcbea07025461ea7b7593025e3796ac7
seojamyeong <seojm0508@gmail.com>
2025-08-28 11:19:23 +0900

    commit

--- src/main/java/kr/co/iei/notice/controller/NoticeController.java
+++ src/main/java/kr/co/iei/notice/controller/NoticeController.java
@@ -20,8 +20,7 @@ public class NoticeController {
 	@GetMapping(value="/list")
 	public String noticeList(Model model) {
 		List list = noticesService.selectNoticeList();
-		
-//		model.addAttribute("list", ListData.getList());
+		model.addAttribute("list",list);
 //		model.addAttribute("pageNav", ListData.getPageNav());
 		return "notice/list";
 	}
